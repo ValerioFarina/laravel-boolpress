1a1310e549c6ff287695aaf38ee45952c9896a3e
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-26 15:29:45 +0100

    Create rotte per Admin

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('index');
 
 Auth::routes();
 
-Route::get('/home', 'Admin/HomeController@index')->middleware('auth')->name('dashboard');
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function() {
+    Route::get('/', 'HomeController@index')->name('index');
+});
