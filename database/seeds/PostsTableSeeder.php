dad99c2a713840a81e360a64f19a939ae2ec7fdc
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-26 14:40:17 +0100

    Modificato seeder per rendere conto delle nuove colonne

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -6,6 +6,8 @@ use App\Post;
 
 use Faker\Generator as Faker;
 
+use Illuminate\Support\Str;
+
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -18,7 +20,18 @@ class PostsTableSeeder extends Seeder
         for ($i = 0; $i < 100; $i++) {
             $new_post = new Post();
             $new_post->title = $faker->sentence();
+            $slug = Str::slug($new_post->title, '-');
+            $new_slug = $slug;
+            $slug_found = Post::where('slug', $new_slug)->first();
+            $counter = 1;
+            while ($slug_found) {
+                $new_slug = $slug . '-' . $counter;
+                $counter++;
+                $slug_found = Post::where('slug', $new_slug)->first();
+            }
+            $new_post->slug = $new_slug;
             $new_post->author = $faker->name();
+            $new_post->content = $faker->text(500);
             $new_post->save();
         }
     }
