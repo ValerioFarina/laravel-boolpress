297d4145985f9de2576aeca6bee345e0aeab5b85
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-27 14:47:35 +0100

    Popolata la tabella categories con dati fake usando il seeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Category;
+
+use Faker\Generator as Faker;
+
+use Illuminate\Support\Str;
+
 class CategoriesTableSeeder extends Seeder
 {
     /**
@@ -9,8 +15,22 @@ class CategoriesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
-    }
+     public function run(Faker $faker)
+     {
+         for ($i = 0; $i < 5; $i++) {
+             $new_category = new Category();
+             $new_category->name = $faker->words(3, true);
+             $slug = Str::slug($new_category->name, '-');
+             $new_slug = $slug;
+             $slug_found = Category::where('slug', $new_slug)->first();
+             $counter = 1;
+             while ($slug_found) {
+                 $new_slug = $slug . '-' . $counter;
+                 $counter++;
+                 $slug_found = Category::where('slug', $new_slug)->first();
+             }
+             $new_category->slug = $new_slug;
+             $new_category->save();
+         }
+     }
 }
