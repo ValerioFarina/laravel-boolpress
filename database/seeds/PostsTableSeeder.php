519827901f8bcee3512c1349fc8b5f61d55541d3
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-27 15:17:50 +0100

    Aggiunto un category_id random per ogni post

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Seeder;
 
 use App\Post;
 
+use App\Category;
+
 use Faker\Generator as Faker;
 
 use Illuminate\Support\Str;
@@ -17,6 +19,12 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $categories = Category::all();
+        $category_ids = [];
+        foreach ($categories as $category) {
+            $category_ids[] = $category->id;
+        }
+
         for ($i = 0; $i < 100; $i++) {
             $new_post = new Post();
             $new_post->title = $faker->sentence();
@@ -32,6 +40,7 @@ class PostsTableSeeder extends Seeder
             $new_post->slug = $new_slug;
             $new_post->author = $faker->name();
             $new_post->content = $faker->text(500);
+            $new_post->category_id = $category_ids[array_rand($category_ids)];
             $new_post->save();
         }
     }
