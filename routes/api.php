0c6b56c9e04ad50e0d8a2e5e6dc2b934d400526d
Valerio Farina <valerio.farina.1994@gmail.com>
2021-02-01 15:34:20 +0100

    Modificato il metodo della rotta "api.posts.index" da GET a POST

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', 'Api\PostController@index')->name('api.posts.index');
+Route::post('/posts', 'Api\PostController@index')->name('api.posts.index');
