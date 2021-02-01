83d0ca3e49515418a6f73f82294f23feb338c3aa
Valerio Farina <valerio.farina.1994@gmail.com>
2021-02-01 16:38:16 +0100

    Definita funzione handle nel middleware

--- app/Http/Middleware/CheckApiToken.php
+++ app/Http/Middleware/CheckApiToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\User;
 
 class CheckApiToken
 {
@@ -15,6 +16,21 @@ class CheckApiToken
      */
     public function handle($request, Closure $next)
     {
+        $auth_token = $request->header('authorization');
+        if (empty($auth_token)) {
+            return response()->json([
+                'success' => false,
+                'error' => 'API token mancante'
+            ]);
+        }
+        $api_token = substr($auth_token, 7);
+        $user = User::where('api_token', $api_token)->first();
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'error' => 'API token errato'
+            ]);
+        }
         return $next($request);
     }
 }
